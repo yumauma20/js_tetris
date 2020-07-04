97f7cb79be32933adbf91e406306e61e4c45103a
yumauma20 <71yuma10@gmail.com>
2020-07-04 18:34:58 +0900

    フィールドの設定

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ let CANVAS_HEIGHT = 600; //canvasの高さ
 let BLOCK_WIDTH = 4; //ブロックの領域幅
 let BLOCK_HEIGHT = 4; //ブロックの領域高さ
 
+let FIELD_WIDTH = 12; //フィールドの幅
+let FIELD_HEIGHT = 22;　//フィールドの高さ
+
+let FIELD_X = 40;　//フィールドのcanvas内のX座標
+let FIELD_Y = 40;　//フィールドのcanvas内のY座標
+
 let block = [ // ブロックの定義
   [	//	block type 0
     [
@@ -202,7 +208,94 @@ let block = [ // ブロックの定義
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
+var field;
+
+var btype, brot;
+var bx, by;
+
+function init() {
+  field = [ //fieldの内容
+    //壁を9とする。何もないマスは0とする。
+    [9, 9, 9, 0, 0, 0, 0, 0, 0, 9, 9, 9,],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9,],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9,],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9,],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9,],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9,],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9,],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9,],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9,],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9,],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9,],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9,],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9,],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9,],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9,],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9,],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9,],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9,],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9,],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9,],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9,],
+    [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9,],
+  ];
+
+  bx = 4; //ブロックのX座標（マス）
+  by = 0; //ブロックのY座標（マス）
+  btype = 0; //ブロックの種類
+  brot = 0; //ブロックの回転種類
+}
+
+//落下ブロックの描画
+function drawBlock() {
+  context.fillStyle = "rgba(255, 100, 100, 1.0)"; //赤色に設定
+
+  //ブロックを描画
+  for(var i = 0; i < BLOCK_HEIGHT; i++) {
+    for(var j = 0; j < BLOCK_WIDTH; j++) {
+      if(block[btype][brot][i][j] == 1) {
+        context.fillRect(FIELD_X + (bx + j) * 25, FIELD_Y + (by + i) * 25, 25, 25);
+      }
+    }
+  }
+}
+
+//	フィールド描画
+function drawField() {
+	for(var i = 0;i < FIELD_HEIGHT;i++) {
+		for(var j = 0;j < FIELD_WIDTH;j++) {
+			if(field[i][j] == 0) continue; // 「０」なら描画しない
+			
+			context.fillStyle = "rgba(150, 150, 150, 1.0)"; // グレーに設定
+			context.fillRect(FIELD_X + j * 25, FIELD_Y + i * 25, 25, 25); // 25×25の矩形（マス）を描画
+		}
+	}
+}
+  
+//フィールドの枠描画
+function drawFrame() {
+  context.fillStyle = "rgba(230; 230, 230, 1.0)";
+
+  //縦線を描画
+  for(var i = 0; i < FIELD_WIDTH + 1; i++) {
+    context.fillRect(FIELD_X + i * 25, FIELD_Y, 1, 25 * FIELD_HEIGHT);
+  }
+
+  //	横線を描画
+  for(var i = 0;i < FIELD_HEIGHT + 1;i++) {
+    context.fillRect(FIELD_X, FIELD_Y + i * 25, 25 * FIELD_WIDTH, 1);
+  }
+}
+
+init();
+
 requestAnimationFrame(main);
 function main() {
+  context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+  drawBlock(); //ブロックを描画
+  drawField(); //フィールドを描画
+  drawFrame(); //フィールド枠を描画
+
   requestAnimationFrame(main);
 }
